3ace69e4df79ac5c360539ed42a68491264c465f
robbyrod5 <robbyrod5@gmail.com>
2015-04-07 19:01:16 -0400

    Added some GUI
    added the first 2 pages

--- code/app/src/main/java/com/joshrobjames/circuitapp/circuitboard.java
+++ code/app/src/main/java/com/joshrobjames/circuitapp/circuitboard.java
@@ -4,24 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.view.View;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class circuitboard extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_circuitboard);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_circuitboard, menu);
         return true;
     }
 
@@ -38,13 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void closeApp(View view) {
-        finish();
-    }
-
-    public void startApp(View view) {
-        Intent startAppIntent = new Intent(this, description.class);
-        startActivity(startAppIntent);
     }
 }
--- code/app/src/main/java/com/joshrobjames/circuitapp/description.java
+++ code/app/src/main/java/com/joshrobjames/circuitapp/description.java
@@ -8,20 +8,19 @@ import android.content.Intent;
 import android.view.View;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class description extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_description);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_description, menu);
         return true;
     }
 
@@ -39,12 +38,8 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void closeApp(View view) {
-        finish();
-    }
-
     public void startApp(View view) {
-        Intent startAppIntent = new Intent(this, description.class);
+        Intent startAppIntent = new Intent(this, circuitboard.class);
         startActivity(startAppIntent);
     }
 }
