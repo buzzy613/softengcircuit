3448bc11d2780bf0ec4283090cd685158591770e
buzzy613 <reddishraddish@gmail.com>
2015-04-14 20:25:06 -0400

    Diode added

---
+++ code/app/src/main/java/com/joshrobjames/circuitapp/Diode.java
@@ -0,0 +1,13 @@
+package com.joshrobjames.circuitapp;
+
+/**
+ * Created by James on 4/14/2015.
+ */
+
+//SOOPER UNFINISHED; DO NOT USE
+public class Diode extends Component {
+
+    public Diode () {
+        super(2);
+    }
+}
